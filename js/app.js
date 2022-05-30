6d4c7742cf3014c0b59af1b49746fbb3ecca0eb5
Semandr <semandr.code@gmail.com>
2022-05-30 11:49:53 +0200

    Write first part of logic for selection rating

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 'use strict';
+const btnSubmit = document.querySelector('.btn__submit');
+const radioBtns = document.querySelectorAll('input[name="rating"]');
+const sectionChoise = document.querySelector('.section__choise');
+const sectionOutput = document.querySelector('.section__output');
+let rating;
+btnSubmit.addEventListener('click', () => {
+  radioBtns.forEach(element => {
+    if (element.checked) rating = element.value;
+  });
+});
